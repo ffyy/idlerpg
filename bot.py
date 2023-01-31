521e18449db2791cde9e9da018df35a2f022e6b2
ffyy <trinkjuhan@gmail.com>
2023-01-31 16:16:48 +0200

    first slash command

--- bot.py
+++ bot.py
@@ -1,29 +1,26 @@
 import os
+import random
 
 import discord
+from discord import app_commands
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('DISCORD_GUILD')
+GUILD_ID = os.getenv('GUILD_ID')
 
 intents = discord.Intents.default()
 intents.members = True
 client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
+
+@tree.command(name = "sup", description = "this should be a slash sup command", guild=discord.Object(id=GUILD_ID))
+async def sup_command(interaction):
+    await interaction.response.send_message("sup bro")
 
 @client.event
 async def on_ready():
-    for guild in client.guilds:
-        if guild.name == GUILD:
-            break
-
-    print(
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})\n'
-    )
-
-    for member in guild.members:
-        print(member)
-
+    await tree.sync(guild=discord.Object(id=GUILD_ID))
+    print("command synced")
 
 client.run(TOKEN)
