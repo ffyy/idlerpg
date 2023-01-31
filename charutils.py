bf584371fa63f62e318adeb15d4aa04cf7aa964b
ffyy <trinkjuhan@gmail.com>
2023-01-31 17:11:23 +0200

    add char utils

---
+++ charutils.py
@@ -0,0 +1,15 @@
+import json
+import os
+
+def saveToDisk(content):
+    try:
+        os.mkdir("chars")
+    except Exception:
+        pass
+
+    f = open(os.path.join("chars", content + ".txt"), "a")
+    f.write(content)
+    f.close()
+
+
+saveToDisk("tuut")
