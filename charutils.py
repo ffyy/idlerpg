c5a1a5610c82df96e339217ac3ffe9406dc2a87a
ffyy <trinkjuhan@gmail.com>
2023-10-18 00:36:23 +0300

    make get_character use actual gear object

--- charutils.py
+++ charutils.py
@@ -28,14 +28,12 @@ def save_to_disk(character: Character):
             print(str(Exception))
 
     file_name = "chars/" + character.name
-    #print(file_name)
     if os.path.isfile(file_name + ".txt"): 
         with open(os.path.join(file_name + ".txt"), "r") as txtfile:
             with open(os.path.join(file_name + ".bak"), "a") as bakfile:
                 bakfile.write('\n\n' + str(datetime.now()) + '\n')
                 shutil.copyfileobj(txtfile, bakfile)
     
-    #print("save_to_disk_says " + str(vars(character)))
     character_json = json.dumps(vars(character), default=lambda x: x.__dict__)
     f = open(os.path.join(file_name + ".txt"), "w")
     f.write(character_json)
@@ -43,8 +41,6 @@ def save_to_disk(character: Character):
 
 def create_character(name):
     new_character = Character(name, 0, 0, Gear(5))
-    #print(json.dumps(vars(new_character)))
-    print(vars(new_character))
     try:
         save_to_disk(new_character)
         return "Saved character called " + new_character.name + " to disk"
@@ -55,18 +51,14 @@ def get_character(name):
     file_path = os.path.join("chars/" + name + ".txt")
     f = open(file_path)
     json_character = json.load(f)
-    #print(json_character) #debug
-    loaded_character = Character(json_character['name'], json_character['level'], json_character['current_xp'], json_character['gear'])
-    print(vars(loaded_character))
-    return "The name is " + loaded_character.name + " and it has gear of level " + str(loaded_character.gear['gearscore'])
+    loaded_character = Character(json_character['name'], json_character['level'], json_character['current_xp'], Gear(json_character['gear']['gearscore']))
+    return "The name is " + loaded_character.name + " and it has gear of level " + str(loaded_character.gear.gearscore)
 
 def update_gear(name, value):
     file_path = os.path.join("chars/" + name + ".txt")
     f = open(file_path)
     json_character = json.load(f)
-    #print(json_character) #debug
     loaded_character = Character(json_character['name'], json_character['level'], json_character['current_xp'], Gear(json_character['gear']['gearscore']))
-    print(vars(loaded_character))
     loaded_character.gear.gearscore += value
     save_to_disk(loaded_character)
     return str(loaded_character.name) + " now has a gearscore of " + str(loaded_character.gear.gearscore)
