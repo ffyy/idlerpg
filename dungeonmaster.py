6aa48ccb4d79682ad2c582aebe882d325ea925f7
ffyy <trinkjuhan@gmail.com>
2023-10-20 01:29:27 +0300

    make party quest together

--- dungeonmaster.py
+++ dungeonmaster.py
@@ -2,14 +2,21 @@ import random
 import charutils
 from rpgobjects import *
 
-def overcome_encounter(adventurer: Character, encounter_difficulty):
-    adventurer_roll = adventurer.roll_dice() + adventurer.gear.gearscore
+def overcome_encounter(party: list[Character], encounter_difficulty):
+    party_roll = 0
+    encounter_results = []
+
+    for adventurer in party:
+        adventurer_roll = adventurer.roll_dice() + adventurer.gear.gearscore
+        party_roll += adventurer_roll
+        encounter_results.append([adventurer.name, str(adventurer_roll)])
     
-    if adventurer_roll >= encounter_difficulty:
-        charutils.level_up_by_id(adventurer.id_)
-        return [1, adventurer_roll]
+    if party_roll >= encounter_difficulty:
+        for adventurer in party:
+            charutils.level_up_by_id(adventurer.id_)
+        return [1, encounter_results]
     else:
-        return [0, adventurer_roll]
+        return [0, encounter_results]
 
 def run_adventure():
     adventurers = []
@@ -25,23 +32,16 @@ def run_adventure():
     else:
         party = random.sample(adventurers, k=len(adventurers)//2) 
     
-    encounter_difficulty = random.randint(1,100)
-    
-    encounter_outcomes = []
-    for adventurer in party:
-        adventurer_outcome = (adventurer.name, overcome_encounter(adventurer, encounter_difficulty))
-        encounter_outcomes.append(adventurer_outcome)
+    encounter_difficulty = random.randint(1,100*len(party))
 
-    adventure_result = "An epic adventure was had! The Encounter difficulty was: " + str(encounter_difficulty) + "\nParticipants and outcomes:"
-    for outcome in encounter_outcomes:
-        adventure_result = '\n'.join([adventure_result, outcome[0]])
-        if outcome[1][0] == 1:
-            adventure_result = ' - '.join([adventure_result, "Success!"])
-            adventure_result = ' | '.join([adventure_result, "Roll: "])
-            adventure_result = ''.join([adventure_result, str(outcome[1][1])])
-        elif outcome[1][0] == 0:
-            adventure_result = ' - '.join([adventure_result, "Failure! "])
-            adventure_result = ' | '.join([adventure_result, "Roll: "])
-            adventure_result = ''.join([adventure_result, str(outcome[1][1])])
+    encounter_outcome = overcome_encounter(party, encounter_difficulty)
+
+    adventure_result = "An epic adventure was had! The Encounter difficulty was: " + str(encounter_difficulty) + "\nOutcome: "
+    if encounter_outcome[0] == 1: adventure_result = ''.join([adventure_result,"Success!"])
+    elif encounter_outcome[0] == 0: adventure_result = ''.join([adventure_result,"Failure!"])
+    adventure_result = ''.join([adventure_result,"\nParticipants and rolls:"])
+    for participant in encounter_outcome[1]:
+        adventure_result = '\n'.join([adventure_result, participant[0]])
+        adventure_result = ' - '.join([adventure_result, participant[1]])
 
     return adventure_result
