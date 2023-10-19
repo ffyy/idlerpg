8068cab6824af8b95e12b51db58f3d9adfe3f612
ffyy <trinkjuhan@gmail.com>
2023-10-19 17:29:27 +0300

    add CharacterDB, move classes to rpgobjects

--- charutils.py
+++ charutils.py
@@ -1,49 +1,13 @@
 import os
 import sqlite3
 import traceback
+from rpgobjects import *
 from dotenv import load_dotenv
 
 load_dotenv()
 DB_PATH = os.getenv("DB_PATH")
 
-class Character:
-    def __init__(
-            self, 
-            id_,
-            name,
-            level,
-            current_xp,
-            class_id,
-            gear_id
-            ):
-        self.id_ = id_
-        self.name = name
-        self.level = level
-        self.current_xp = current_xp
-        self.class_id = class_id
-        self.gear_id = gear_id
-
-class Gear:
-    def __init__(
-            self,
-            id_,
-            gearscore
-            ):
-        self.id_ = id_
-        self.gearscore = gearscore
-
-class CharacterClass:
-    def __init__(
-            self,
-            id_,
-            name,
-            tactic
-    ):
-        self.id_ = id_
-        self.name = name
-        self.tactic = tactic
-
-def save_to_db(character: Character):
+def save_to_db(character: CharacterDB):
     db = sqlite3.connect(DB_PATH)
     cur = db.cursor()
     if (character.id_ is None):
@@ -60,12 +24,12 @@ def get_from_db(name):
     cur = db.cursor()
     cur.execute("SELECT * FROM character WHERE name = ?", (name,))
     db_character = cur.fetchone()
-    character = Character(db_character[0], db_character[1], db_character[2], db_character[3], db_character[4], db_character[5])
+    character = CharacterDB(db_character[0], db_character[1], db_character[2], db_character[3], db_character[4], db_character[5])
     return character
 
 def create_character(name, class_id):
-    new_character = Character(None, name, 0, 0, class_id, None)
-    print(str(vars(new_character)))
+    new_character = CharacterDB(None, name, 0, 0, class_id, None)
+    #class_name = get_db_class(class_id).name
     try:
         save_to_db(new_character)
         return "Saved character called " + new_character.name + " with class id " + str(new_character.class_id) + " to database"
---
+++ rpgobjects.py
@@ -0,0 +1,68 @@
+class Gear:
+    def __init__(
+            self,
+            id_,
+            gearscore
+            ):
+        self.id_ = id_
+        self.gearscore = gearscore
+
+class CharacterClass:
+    def __init__(
+            self,
+            id_,
+            name,
+            tactic
+    ):
+        self.id_ = id_
+        self.name = name
+        self.tactic = tactic
+
+class Character:
+    def __init__(
+            self, 
+            id_,
+            name,
+            level,
+            current_xp,
+            character_class: CharacterClass,
+            gear: Gear,
+            #player_id
+            ):
+        self.id_ = id_
+        self.name = name
+        self.level = level
+        self.current_xp = current_xp
+        self.character_class = character_class
+        self.gear = gear
+        #self.player_id = player_id
+
+class CharacterDB:
+    def __init__(
+            self, 
+            id_,
+            name,
+            level,
+            current_xp,
+            class_id,
+            gear_id#,
+            #player_id
+            ):
+        self.id_ = id_
+        self.name = name
+        self.level = level
+        self.current_xp = current_xp
+        self.class_id = class_id
+        self.gear_id = gear_id
+        #self.player_id = player_id
+
+class Player:
+    def __init__(
+            self,
+            id_,
+            discord_id,
+            character_id
+    ):
+        self.id_ = id_
+        self.discord_id = discord_id
+        self.character.id = character_id
