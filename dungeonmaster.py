b17ed69ecce068be4552ab409feae2ce6bc5c4d6
ffyy <trinkjuhan@gmail.com>
2023-10-23 22:23:49 +0300

    move make_table to separate helpers module

--- dungeonmaster.py
+++ dungeonmaster.py
@@ -1,6 +1,7 @@
 import random
 import charutils
 from rpgobjects import *
+from helpers import *
 
 QUEST_HOOKS = open("content/quests.txt").read().splitlines()
 SUCCESS_DESCRIPTIONS = open("content/successes.txt").read().splitlines()
@@ -145,56 +146,3 @@ def run_long_rest():
         string_day_report = make_table(table_lists)
 
         return string_day_report
-
-
-#helper function because discord embeds arent very cool
-def make_table(input) -> str:
-    column_widths = []
-
-    for i,column in enumerate(input):
-        column_widths.append(0)
-        for subitem in input[i]:
-            if len(subitem) > column_widths[i]:
-                column_widths[i] = len(subitem)
-
-    #top line
-    table = "+"
-    for column_width in column_widths:
-        table = "".join([table, "-"*(column_width+2)])
-        table = "".join([table, "+"])
-    table = "".join([table, "\n"])
-
-    #headers
-    for i,column in enumerate(input):
-        table = "".join([table, "|"])
-        table = " ".join([table, column[0]])
-        table = "".join([table, " "*(column_widths[i] + 1 - len(column[0]))])
-    table = "".join([table, "|\n+"])
-    
-    #line below
-    for column_width in column_widths:
-        table = "".join([table, "-"*(column_width+2)])
-        table = "".join([table, "+"])
-    table = "".join([table, "\n"])
-
-    #rows
-    for header in input:
-        del header[0]
-
-    while len(input[0]) > 0:
-        for i, column in enumerate(input):
-            table = "".join([table, "|"])
-            table = " ".join([table, column[0]])
-            table = "".join([table, " "*(column_widths[i] + 1 - len(column[0]))])
-            del column[0]
-        table = "".join([table, "|\n"])
-    
-    #bottom line
-    table = "".join([table, "+"])
-    for column_width in column_widths:
-        table = "".join([table, "-"*(column_width+2)])
-        table = "".join([table, "+"])
-    
-    print(table)
-
-    return(table)
