bf56fb9351dcc1fe88e50cbac268de18766f4a1d
ffyy <trinkjuhan@gmail.com>
2023-10-29 11:58:01 +0200

    add /rules command

--- bot.py
+++ bot.py
@@ -254,6 +254,81 @@ async def changename(interaction: discord.Interaction, new_name: str):
     else:
         await interaction.response.send_message(content="Something went wrong", ephemeral=True)
 
+class RulesView(discord.ui.View):
+    def __init__(self):
+        super().__init__(timeout=100)
+
+    rules_sections = [
+        discord.SelectOption(label="General", value="0", description="General game rules"),
+        discord.SelectOption(label="Experience", value="1", description="How do characters gain XP?"),
+        discord.SelectOption(label="Questing", value="2", description="How are are quests run?"),
+        discord.SelectOption(label="Gear", value="3", description="What does gear do?"),
+        discord.SelectOption(label="PvP", value="4", description="How do characters fight each other?")
+    ]
+
+    @discord.ui.select(placeholder="Rules section", options=rules_sections, max_values=1)
+    async def reply_select(self, interaction: discord.Interaction, select: discord.ui.Select):
+        #response = next((line for line in RULES_TEXTS if line[0] == select.values[0]), "  Oops")[2:]
+        match select.values[0]:
+            case "0":
+                response = """
+In this game, you register a character and watch it take part in heroic adventures without any further input from you. The only control you have over your character is registering, deleting, and changing its name.\n
+Each real-life hour, a day passes in the game world. During each game day:
+- a single random character will solve a small personal class-specific quest
+- a group of characters will embark on a difficult adventure
+- a lower-level character will attempt to fight a higher-level character
+- all characters will take a rest.\n
+The choice of class when registering has a large impact on how your character will perform. Different classes use different dice when rolling to determine the outcomes of challenges. Classes can also have different maximum hit points values and different experience targets for leveling up.\n
+Losing at challenges will hurt your character. If a character's hit points (HP) run out, they will permanently die and will be reincarnated at level 0, with the same name and class.\n
+If a character has earned enough experience to reach the next level, they will level up and heal to full when resting.
+                """
+            case "1":
+                response = """
+Characters can gain experience in the following ways: by completing a personal quest, by succeeding at a group quest, by ganking someone in PvP and passively throughout the day.\n
+- **Personal quests**: Between 500 and 2000 XP is randomly awarded simply for being the character chosen to undertake a personal quest. This selection and the xp reward from it is the only roll in the game which doesn't use the class specific dice to determine its outcome.
+- **Group quests**: XP is awarded based on the difficulty of the quest. The quest difficulty is randomly determined by the DM. The maximum XP reward is 10000 (enough for one entire level for a class with average leveling speed) and scales linearly with the average target number each party member should hit. For example, if the difficulty for a two-character quest is 200, it means both characters would need to roll a 100 to win, and they would be awarded 10000 xp each. If the quest difficulty for the same party were 60, it would mean the average target would be 30, and the xp reward would be 10000*30/100 = 3000 xp each.
+- **PvP**: XP is awarded based on the level difference between the ganker and the defender. 1000 xp, up to a maximum of 10000, is awarded per each level the defender has over the ganker. The minimum xp from winning a PvP encounter is 1000, even when fighting a character of the same level.
+- **Passive XP**: Each time characters rest, all characters roll to determine how much passive xp they gained during the day. The maximum amount of xp earned passively each day is 3600 and it scales linearly with the result of the passive xp roll. The class-specific dice are used for this roll, but gearscore is not used. For example, since the maximum roll a Hobbit can get is 50 (a result of 10 on each of their 5 dice), a Hobbit can gain at most 1800 xp passively each day.
+"""
+            case "2":
+                response = """
+When the DM runs a quest, an adventuring party is first randomly put together. If multiple characters exist in the world, the party size ranges from 2 to half of all characters. All characters are always eligible to go on quests.\n
+The DM randomly determines the difficulty of the quest. The quest difficulty scales with party size. The DM determines the minimum average roll for the party members and multiplies it by the number of characters in the party. The minimum difficulty per party member is 20 and the maximum is 100.\n
+Suceeding at the quest always awards all characters in the party with experience. If the party rolls very well, a magic item which increases gearscore can also be awarded.
+- For specifics on the xp calculation, see the 'Experience' section of the rules.
+- For specifics on the item rewards, see the 'Gear' section of the rules.
+- If the party includes any Clerics, succeeding at a quest also heals all members of the party. With the exception of leveling up, this is the only way to regain lost hit points.\n
+Failure at the quest awards nothing. All characters involved in the quest also lose hit points. The amount of hp lost scales linearly with the ratio of the party's total roll to the quest difficulty, with a theoretical maximum of 100. For example, if the quest difficulty was 100 and the party rolls total was 65, all members of the party will lose 35 hp.
+"""
+            case "3":
+                response = """
+Gear and gearscore are an abstraction of all the magical items the character has acquired throughout their adventuring career. The character's gearscore value is used in rolls which determine success at encounters (group quests and PvP).
+Items can be gained in the following ways:
+- **Be a Hobbit**: All Hobbits start with a gearscore of 20, because all adventuring Hobbits have a powerful magic ring.
+- **Do well at party quests**: If the party succeeds at a group quest, there is a chance that the party member with the highest roll is awarded a magic item. The chance for an item to be awarded scales with the ratio of the party's total roll compared to the quest difficulty, with a 100% chance if the party's total roll is double the quest difficulty, going down to 1% if the party just barely met the target number. A magic item is also always awarded if the party succeeds at the quest and the average roll in the party is above 80. For example, for a four-character party, if the total party roll is at least 320, an item is guaranteed to be awarded, as long as that roll was enough to beat the quest difficulty.
+- **Kill other characters**: If a character dies in a PvP encounter, the killer is guaranteed at least one magic item. If the character who died had a gearscore higher than 1, a random number of items, up to the dead character's gearscore, is awarded to the killer.\n
+Items which are found by characters are not immediately equipped. When a character finds an item, it is considered unattuned. Magic items are only equipped (the character's gearscore increased) during long rests.
+                """
+            case "4":
+                response = """
+PvP is a high-risk/high-reward catch-up activity. In PvP encounters, a character who is falling behind in levels attacks a character ahead of them in an attempt to gain bonus experience.\n
+If there are enough characters in the world for tensions to develop, every game day, a PvP encounter is triggered. Only characters in the bottom half of the leaderboard are eligible to trigger PvP encounters. All characters ahead of them on the leaderboard are possible targets.\n
+In the PvP encounter, the two characters roll off directly against each other.
+- For the roll, class dice plus gearscore is used.
+- The character with the higher roll wins the encounter.
+- The losing character loses hp equal to the difference in rolls.
+- If any character dies in the encounter, the winner loots at least one magic item from the corpse. See the 'Items' section of the rules for more info.
+- If the instigator wins the encounter, they earn bonus experience. See the 'Experience' section of the rules for more info.
+"""
+            case _:
+                response = "Tunnel snakes rule"
+        response = "\n".join([response, "Select the next section to read:"])
+        await interaction.response.edit_message(content=response)
+
+@client.tree.command(name="rules", description="Display game rules")
+async def rules(interaction: discord.Interaction):
+    await interaction.response.send_message("Choose a section of rules to read.", view=RulesView(), ephemeral=True)
+
 #LOOP FUNCTIONS
 def create_rest_report():
     rest_results = dungeonmaster.run_long_rest()
